bc760f8f66796c2bf596c1b86e72247fe4418891
sb01043@naver.com <sb01043@naver.com>
2021-06-17 10:04:27 +0900

    js 추가파일

--- js/event.js
+++ js/event.js
@@ -2,9 +2,15 @@
 function showtext(){
     var text = document.getElementById("desc");
     text.style.display = "block";
+
+    var button = document.getElementById("btn");
+    button.style.display = "none";
 }
 
 function hidetext(){
     var text = document.getElementById("desc");
     text.style.display = "none";
+
+    var button = document.getElementById("btn");
+    button.style.display = "block";
 }
